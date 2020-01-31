5fe1d6ae6d2a4f5e82858dbf6e961aee2895eec2
cnichols <cnichols@rcentral.org>
2020-01-30 20:22:10 -0600

    Add gui functionality. Add listbox.

--- frontend_gui.py
+++ frontend_gui.py
@@ -26,6 +26,20 @@ class Window(Frame):
     def __init__(self,master=None):
         Frame.__init__(self,master)
         self.master = master
+        self.left_frame = Frame(self.master)
+        self.left_frame.pack(side=LEFT)
+        self.right_frame = Frame(self.master)
+        self.right_frame.pack(side=RIGHT)
+
+        self.current_student = None
+        if self.current_student is None:
+            self.current_student = backend.Student("None Selected",12)
+
+
+        self.student_label_text = StringVar()
+        self.student_label_text.set(f"Student Name: {self.current_student.name}")
+
+        self.student_label = Label(self.right_frame, textvariable=self.student_label_text)
         self.init_window()
 
     def init_window(self):
@@ -40,13 +54,16 @@ class Window(Frame):
         self.menubar.add_cascade(label="Reports", menu=self.report_menu)
         self.master.config(menu=self.menubar)
 
-        self.listbox = Listbox(self.master)
-        self.listbox.pack()
+        self.listbox = Listbox(self.left_frame,height = 50, width=100,selectmode=SINGLE)
+        self.listbox.grid(column=0,row=0)
         self.listbox.insert(END, "a list entry")
         self.refresh_students_listbox()
 
-        self.load_button = Button(self.master,text="Load", command=self.load_student)
-        self.load_button.pack()
+        self.load_button = Button(self.left_frame,text="Load", command=self.load_student)
+        self.load_button.grid(column=1,row=0)
+
+        self.student_label = Label(self.right_frame, textvariable=self.student_label_text)
+        self.student_label.pack()
 
     def refresh_students_listbox(self, sort_parameter=None,criteria=None):
         students = backend.return_student_list(sort_parameter, criteria)
@@ -59,8 +76,18 @@ class Window(Frame):
         self.listbox.delete(0, END)
         for name in student_names:
             self.listbox.insert(END, name)
+
     def load_student(self):
-        print(self.listbox.get(0))
+        temp_student = self.listbox.get(self.listbox.curselection()[0])
+        print(temp_student)
+        if temp_student:
+            print(temp_student)
+            self.current_student = backend.search_table(temp_student)
+            self.student_label_text.set(f"Student Name: {self.current_student.name}")
+        else:
+            print("Nope")
+            self.student_label_text.set(f"Student Name: None Selected")
+
 
 # TODO: Add filter, keep working on listbox function, then add info area on right side
 root.state("zoomed")
