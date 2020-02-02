6ed763434ebd1fda42257d312421e0ca7d57fedd
cnichols <cnichols@rcentral.org>
2020-02-01 21:32:03 -0600

    Add gui functionality. Add listbox.

--- frontend_gui.py
+++ frontend_gui.py
@@ -40,6 +40,10 @@ class Window(Frame):
             self.current_student.student_id = None
             self.current_student.grade = None
 
+        self.add_name_text = StringVar()
+        self.add_grade_int = IntVar()
+        self.add_csa_hours_flt = DoubleVar()
+        
         self.init_info()
         self.init_window()
 
@@ -79,16 +83,21 @@ class Window(Frame):
         self.report_menu.add_command(label="Export Program Report", command=backend.generate_program_report)
         self.menubar.add_cascade(label="Reports", menu=self.report_menu)
 
-        self.menubar.add_command(label="Graduate Levels", command=self.graduate_levels_reload)
+        self.menubar.add_command(label="Refresh", command=self.reload_ui)
         self.master.config(menu=self.menubar)
 
-        self.listbox = Listbox(self.left_frame, height=50, width=100, selectmode=SINGLE)
-        self.listbox.grid(column=0, row=0)
+        # self.listbox = Listbox(self.left_frame, height=50, width=100, selectmode=SINGLE)\
+        self.listbox = Listbox(self.left_frame,selectmode=SINGLE)
+        self.listbox.grid(column=0, row=0, columnspan=2)
         self.listbox.insert(END, "a list entry")
         self.refresh_students_listbox()
 
+        self.add_button = Button(self.left_frame, text="Add", command=self.add_student_dialog)
+        self.add_button.grid(column=0, row=1)
+
         self.load_button = Button(self.left_frame, text="Load", command=self.load_student)
-        self.load_button.grid(column=1, row=0)
+        self.load_button.grid(column=1, row=1)
+
 
         self.student_name_label = Label(self.right_frame, textvariable=self.student_name_label_text)
         self.student_name_label.grid(column=0, row=0)
@@ -106,6 +115,10 @@ class Window(Frame):
         self.student_csa_hours_label.grid(column=0, row=4)
 
     def refresh_students_listbox(self, sort_parameter=None, criteria=None):
+        try:
+            prev_sel = self.listbox.curselection()
+        except IndexError:
+            prev_sel = 0
         students = backend.return_student_list(sort_parameter, criteria)
         if students is None:
             student_names = ["No data found."]
@@ -116,30 +129,78 @@ class Window(Frame):
         self.listbox.delete(0, END)
         for name in student_names:
             self.listbox.insert(END, name)
+        try:
+            self.listbox.selection_set(prev_sel)
+        except TclError:
+            self.listbox.selection_set(0)
 
     def load_student(self):
-        temp_student = self.listbox.get(self.listbox.curselection()[0])
-        print(temp_student)
-        if temp_student:
+        self.refresh_students_listbox()
+        backend.graduate_csa_levels()
+        try:
+            temp_student = self.listbox.get(self.listbox.curselection()[0])
             print(temp_student)
-            self.current_student = backend.search_table(temp_student)
-            print(self.current_student.csa_level)
-            self.student_name_label_text.set(f"Student Name: {self.current_student.name}")
-            self.student_id_label_text.set(f"Student ID: {self.current_student.student_id}")
-            self.student_grade_label_text.set(f"Student Grade: {self.current_student.grade}")
-            self.student_csa_level_label_text.set(f"Student CSA Level: {self.current_student.csa_level}")
-            self.student_csa_hours_label_text.set(f"Student CSA Hours: {self.current_student.csa_hours}")
-        else:
-            print("Nope")
-            self.student_name_label_text.set(f"Student Name: None Selected")
+            if temp_student:
+                print(temp_student)
+                self.current_student = backend.search_table(temp_student)
+                print(self.current_student.csa_level)
+                self.student_name_label_text.set(f"Student Name: {self.current_student.name}")
+                self.student_id_label_text.set(f"Student ID: {self.current_student.student_id}")
+                self.student_grade_label_text.set(f"Student Grade: {self.current_student.grade}")
+                self.student_csa_level_label_text.set(f"Student CSA Level: {self.current_student.csa_level}")
+                self.student_csa_hours_label_text.set(f"Student CSA Hours: {self.current_student.csa_hours}")
+            else:
+                print("Nope")
+                self.student_name_label_text.set(f"Student Name: None Selected")
+        except IndexError:
+            return None
 
-    def graduate_levels_reload(self):
+    def reload_ui(self):
         backend.graduate_csa_levels()
         self.load_student()
+        self.refresh_students_listbox()
+
+    def add_student_dialog(self):
+        self.add_window = Toplevel()
+        
+        name_label = Label(self.add_window,text="Name")
+        name_label.grid(column=0,row=0)
+        self.add_name_text = StringVar()
+        name_input = Entry(self.add_window, textvariable=self.add_name_text)
+        name_input.grid(column=1,row=0)
+
+        grade_label = Label(self.add_window, text="Grade")
+        grade_label.grid(column=0, row=1)
+        self.add_grade_int = IntVar()
+        grades = {9,10,11,12}
+        grade_input = OptionMenu(self.add_window, self.add_grade_int, *grades)
+        self.add_grade_int.set(9)
+        grade_input.grid(column=1, row=1)
+
+        csa_hours_label = Label(self.add_window, text="CSA Hours")
+        csa_hours_label.grid(column=0, row=2)
+        self.add_csa_hours_flt = DoubleVar()
+        self.add_csa_hours_flt.set(0)
+        csa_hours_input = Entry(self.add_window, textvariable=self.add_csa_hours_flt)
+        csa_hours_input.grid(column=1, row=2)
+        
+        add_button = Button(self.add_window, text="Add Student", command=self.add_student_wrapper)
+        add_button.grid(column=0, row=5)
+        
+    def add_student_wrapper(self):
+        student = backend.Student(name=self.add_name_text.get(),
+                                  grade=self.add_grade_int.get(),
+                                  csa_hours=self.add_csa_hours_flt.get())
+        backend.add_student(student)
+        self.add_window.destroy()
+        self.refresh_students_listbox()
+        self.reload_ui()
 
 # TODO: Add filter, keep working on listbox function, then add info area on right side
 
-root.state("zoomed")
+# root.state("zoomed")
 
+
+root.resizable(0, 0)
 app = Window(root)
 root.mainloop()
