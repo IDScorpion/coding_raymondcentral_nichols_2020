692b4a9222f68e1e93840223209b46e0db7ce94e
cnichols <cnichols@rcentral.org>
2020-02-02 19:29:14 -0600

    Add gui functionality. Add listbox.

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup
 import os
 
-thelibFolder = os.path.dirname(os.path.realpath(__file__))
-requirementPath = thelibFolder + '/requirements.txt'
-install_requires = [] # Examples: ["gunicorn", "docutils>=0.3", "lxml==0.5a7"]
+working_dir = os.path.dirname(os.path.realpath(__file__))
+requirementPath = working_dir + '/requirements.txt'
+install_requires = []
 if os.path.isfile(requirementPath):
     with open(requirementPath) as f:
         install_requires = f.read().splitlines()
 setup(
     name='coding_raymondcentral_nichols_2020',
+    install_requires=install_requires,
     version='1.0',
     packages=[''],
     url='',
